08666f6cd1ad999eaf194ed98b12ca61e06b3b06
DaniloCossioloDias <danilocossiolodiass@gmail.com>
2025-03-22 13:25:49 -0300

    adicionar produtos ao carrinho

--- ifood/src/app/produtos/produtos.page.ts
+++ ifood/src/app/produtos/produtos.page.ts
@@ -2,28 +2,32 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
-import { IonNav } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular'; 
+import { CartService } from '../services/cart.service'; 
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.page.html',
   styleUrls: ['./produtos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonItem, CommonModule]
+  imports: [CommonModule, IonicModule]  
 })
 export class ProdutosPage implements OnInit {
   restaurante: any;
   produtos: any[] | undefined;
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute, private cartService: CartService) {}
 
   ngOnInit() {
-    // Acessar o estado da navegação
     const navigation = this.router.getCurrentNavigation();
     if (navigation?.extras.state) {
-      this.restaurante = navigation.extras.state['restaurante'];  // Acessando o restaurante do estado
+      this.restaurante = navigation.extras.state['restaurante'];
       this.produtos = this.restaurante.produtos;
     }
+  }
+
+
+  adicionarAoCarrinho(produto: any) {
+    this.cartService.adicionarAoCarrinho(produto);
   }
 }
