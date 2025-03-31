1365a48153ba3c137a1e80e814bbd385c02135f3
Felipe <felypebueno2004@gmail.com>
2025-03-31 19:48:12 -0300

    feat: adiciona endpoint de buscar todos os restaurantes

--- server/ifood/src/main/java/br/ifood/controllers/RestaurantController.java
+++ server/ifood/src/main/java/br/ifood/controllers/RestaurantController.java
@@ -1,8 +1,12 @@
 package br.ifood.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.ifood.models.Restaurant;
 import br.ifood.services.RestaurantService;
 
 @RequestMapping(path = "/restaurant")
@@ -12,5 +16,14 @@ public class RestaurantController {
 
     public RestaurantController(RestaurantService restaurantService) {
         this.restaurantService = restaurantService;
+    }
+
+    /**
+     * Endpoint para retornar a lista de restaurantes.
+     * A lógica de acessar o banco e demais tratamentos na lista de retorno
+     * deve estar contigo no método `getAll` do RestaurantService.
+     */
+    public ResponseEntity<List<Restaurant>> getAll() {
+        return ResponseEntity.ok(this.restaurantService.getAll());
     }
 }
--- server/ifood/src/main/java/br/ifood/services/RestaurantService.java
+++ server/ifood/src/main/java/br/ifood/services/RestaurantService.java
@@ -1,7 +1,10 @@
 package br.ifood.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import br.ifood.models.Restaurant;
 import br.ifood.repositories.RestaurantRepository;
 
 @Service
@@ -10,5 +13,12 @@ public class RestaurantService {
 
     public RestaurantService(RestaurantRepository restaurantRepository) {
         this.restaurantRepository = restaurantRepository;
+    }
+
+    /**
+     * Lista todos os restaurantes cadastrados no sistema.
+     */
+    public List<Restaurant> getAll() {
+        return this.restaurantRepository.findAll();
     }
 }
