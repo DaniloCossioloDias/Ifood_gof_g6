9257a15a8b7cd9e6fb42ab394c5f2f26f2c21a25
Exiled Spirit <gab.gabriel.1002@outlook.com>
2025-04-03 17:49:16 -0300

    feat: cria endpoint de novo restaurante

--- server/ifood/src/main/java/br/ifood/controllers/RestaurantController.java
+++ server/ifood/src/main/java/br/ifood/controllers/RestaurantController.java
@@ -3,10 +3,13 @@ package br.ifood.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ifood.models.Restaurant;
+import br.ifood.payloads.restaurant.RestaurantDataPayload;
 import br.ifood.services.RestaurantService;
 
 @RequestMapping(path = "/restaurant")
@@ -25,5 +28,17 @@ public class RestaurantController {
      */
     public ResponseEntity<List<Restaurant>> getAll() {
         return ResponseEntity.ok(this.restaurantService.getAll());
+    }
+
+    /**
+     * Método post que cria um novo restaurante.
+     * 
+     * @param restaurantDataPayload Objeto contendo apenas os dados necessários
+     *                              para a criação de um restaurante.
+     * @return Retorna o restaurante criado.
+     */
+    @PostMapping("/new")
+    public ResponseEntity<Restaurant> create(@RequestBody RestaurantDataPayload restaurantDataPayload) {
+        return ResponseEntity.ok(this.restaurantService.create(restaurantDataPayload));
     }
 }
--- server/ifood/src/main/java/br/ifood/services/RestaurantService.java
+++ server/ifood/src/main/java/br/ifood/services/RestaurantService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import br.ifood.models.Restaurant;
+import br.ifood.payloads.restaurant.RestaurantDataPayload;
 import br.ifood.repositories.RestaurantRepository;
 
 @Service
@@ -20,5 +21,17 @@ public class RestaurantService {
      */
     public List<Restaurant> getAll() {
         return this.restaurantRepository.findAll();
+    }
+
+    /**
+     * Recebe um objeto de payload (contendo apenas os dados necessários) e cria
+     * um novo dado de restaurante no banco de dados.
+     * 
+     * @param restaurantDataPayload Objeto payload contendo os dados necessários
+     *                              para a criação do restaurante.
+     * @return Novo restaurante.
+     */
+    public Restaurant create(RestaurantDataPayload restaurantDataPayload) {
+        return this.restaurantRepository.save(new Restaurant(restaurantDataPayload));
     }
 }
